601e1fb3246dfbca9d26cba60e404ec61571e4b5
yazidjanati <janati.yazid@gmail.com>
2023-08-28 23:11:24 +0200

    emc working with blackjax + mcmc not working somehow

--- tests/emc_test.py
+++ tests/emc_test.py
@@ -1,5 +1,6 @@
 import jax.numpy as jnp
 import jax
+from jax.lax import fori_loop
 from gaussian_mixture import Gaussian_Mixture
 from entropic_mirror_mc import emc, MCMC_kernel
 from jax.tree_util import Partial as partial
@@ -9,31 +10,40 @@ from jax import grad, vmap
 from jax.random import split, PRNGKey, normal, categorical
 from numpyro.distributions import MultivariateNormal, MixtureSameFamily, Categorical, MultivariateStudentT
 import matplotlib.pyplot as plt
-import os
+import blackjax
 from jax import default_device, devices
 
-os.environ['PATH'] = '/usr/local/cuda-11.6/bin'
-os.environ['JAX_LOG_COMPILES'] = '1'
-device_num = 0
-with default_device(devices("cpu")[device_num]):
+def mala_loop(key, init_state, kernel, steps):
+    keys = split(key, steps)
+
+    def one_step(i, state):
+        state, _ = kernel.step(keys[i], state)
+        return state
+
+    return fori_loop(0, steps, one_step, kernel.init(init_state))
+
+with default_device(devices("cpu")[0]):
     dim = 2
     heavy_distr = MultivariateStudentT(df=2., loc=jnp.zeros(dim), scale_tril=jnp.eye(dim))
     cov = jnp.array([[10., 1.], [-5., 1.]])
-    target = mog2_blockdiag_cov(dim=dim, means=[0., 7.], mini_cov=.5*jnp.eye(2),
-                                weights=jnp.array([.5, .5]))
+    target = mog2_blockdiag_cov(dim=dim, means=[0., 10.], mini_cov=cov,
+                                weights=jnp.array([.05, .95]))
     # means = jnp.array([[0., 0.], [5., 5.]])
     # covs = .2 * jnp.eye(dim)[jnp.newaxis, :].repeat(2, 0)
     # target = MixtureSameFamily(Categorical(jnp.array([.9, .1])), MultivariateNormal(means, covs))
     rwm_params = RWM(cov=jnp.eye(dim))
-
+    bkjx_mala = blackjax.mala(target.log_prob, step_size=1e-1)
+    partial_mala = partial(mala_loop, kernel=bkjx_mala, steps=1000)
     partial_rwm = partial(random_walk_mh, logpdf=target.log_prob, params=rwm_params, burn_in_steps=100, steps=1)
     partial_ula = partial(unadjusted_langevin, grad_logpdf=grad(lambda x: target.log_prob(x)), burn_in_steps=100,
                           steps=1,
                           step_size=5e-2)
-    global_kernel = lambda keys, state: vmap(partial_ula, in_axes=(0, 0))(keys, state).reshape(-1, state.shape[-1])
-    local_kernel = lambda keys, state: vmap(partial_rwm, in_axes=(0, 0))(keys, state).reshape(-1, state.shape[-1])
 
-    key_emc = PRNGKey(0)
+    global_kernel = lambda keys, state: vmap(partial_ula, in_axes=(0, 0))(keys, state).reshape(-1, state.shape[-1])
+    # local_kernel = lambda keys, state: vmap(partial_rwm, in_axes=(0, 0))(keys, state).reshape(-1, state.shape[-1])
+    local_kernel = lambda keys, state: vmap(partial_mala, in_axes=(0, 0))(keys, state)[0].reshape(-1, state.shape[-1])
+
+    key_emc = PRNGKey(1)
     model = Gaussian_Mixture(dim=dim, n_components=2)
 
     with jax.disable_jit(False):
@@ -45,7 +55,7 @@ with default_device(devices("cpu")[device_num]):
                        mixed_proposal_weights=jnp.array([.9, .1]))
 
     print(
-        f'mean: {proposal.component_distribution.mean}\n covs: {proposal.component_distribution.covariance_matrix} \n weights: {proposal.mixing_distribution.probs}')
+        f'mean: {proposal.component_distribution.mean}\n covs: {proposal.component_distribution.covariance_matrix}\n weights: {proposal.mixing_distribution.probs}')
     key_proposal, key_target = split(key_emc, 2)
     proposal_samples = proposal.sample(key_proposal, (1000,))
     target_samples = target.sample(key_target, (1000,))
--- tests/mh_test.py
+++ tests/mh_test.py
@@ -1,36 +1,68 @@
 import jax
 import jax.numpy as jnp
-from jax import grad, default_device, devices
-
+from jax.lax import fori_loop
+from jax import grad, default_device, devices, vmap
 import os
 from numpyro.distributions import MultivariateNormal
 from mcmc import random_walk_mh, RWM, MALA, adjusted_langevin
 from jax.random import split, PRNGKey, normal
 import matplotlib.pyplot as plt
+from jax.tree_util import Partial as partial
+from targets import mog2_blockdiag_cov
+import blackjax
+
+# os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+# print(jax.local_devices())
+# print(devices('gpu')[0])
+def mala_loop(key, init_state, kernel, steps):
+    keys = split(key, steps)
+
+    def one_step(i, state):
+        state, _ = kernel.step(keys[i], state)
+        return state
+
+    return fori_loop(0, steps, one_step, kernel.init(init_state))
 
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-print(jax.local_devices())
-print(devices('gpu')[0])
-with default_device(devices('gpu')[0]):
-    dim = 2
-    a = jnp.ones((100, 100))
-    print(a * a)
-    mvn = MultivariateNormal(jnp.zeros(dim), jnp.array([[6., 1.], [3, 1.]]))
+with default_device(devices('cpu')[0]):
+    with jax.disable_jit():
+        dim = 2
+        cov = jnp.array([[10., 1.], [-5., 1.]])
+        target = mog2_blockdiag_cov(dim=dim, means=[0., 10.], mini_cov=cov,
+                                    weights=jnp.array([.5, .5]))
+        init_distr = MultivariateNormal(-5. + jnp.zeros(dim), jnp.eye(dim))
 
-    rwm_params = RWM(cov=jnp.eye(dim))
-    mala_params = MALA(step_size=1e-1, grad_logpdf=lambda x: grad(mvn.log_prob)(x).sum())
+        rwm_params = RWM(cov=jnp.eye(dim))
+        mala_params = MALA(step_size=1e-1, grad_logpdf=lambda x: grad(target.log_prob)(x))
 
-    key_rwm, key_init, key_target = split(PRNGKey(2), 3)
-    init_state = normal(key_init, (dim,))
-    rwm_samples = random_walk_mh(key_rwm, init_state, logpdf=mvn.log_prob, params=rwm_params, burn_in_steps=30, steps=1000)
-    mala_samples = adjusted_langevin(key_rwm, init_state, logpdf=mvn.log_prob, params=mala_params, burn_in_steps=30,
-                                     steps=1000)
-    print(rwm_samples)
-    target_samples = mvn.sample(key_target, (1000,))
+        key_rwm, key_init, key_target = split(PRNGKey(2), 3)
+        init_state = normal(key_init, (dim,))
+        partial_rwm = partial(random_walk_mh, logpdf=target.log_prob, params=rwm_params, burn_in_steps=1000, steps=1)
+        partial_mala = partial(adjusted_langevin, logpdf=target.log_prob, steps=1, params=mala_params, burn_in_steps=1000)
+        rwmh = vmap(partial_rwm, in_axes=(0, 0))
+        mala = vmap(partial_mala, in_axes=(0, 0))
 
-    plt.figure(figsize=(10,5))
-    plt.scatter(target_samples[:,0], target_samples[:,1])
-    plt.scatter(rwm_samples[:,0], rwm_samples[:,1])
-    plt.scatter(mala_samples[:,0], mala_samples[:,1])
-    plt.show()
+        n_chains = 100
+        key = PRNGKey(1234)
+        key_init, key_bkjx, key_rwm, key_mala = split(key, 4)
+        key_rwm, key_mala = split(key_rwm, n_chains), split(key_mala, n_chains)
+        init_samples = target.sample(key_init, (n_chains,))
+        target.log_prob(init_samples)
+
+        bkjx_mala = blackjax.mala(target.log_prob, step_size=1e-1)
+        # init_sample = init_distr.sample(key_init)
+        # mala_loop(key_init, init_sample, kernel=bkjx_mala, steps=1000)
+        bkjx_mala = vmap(partial(mala_loop, kernel=bkjx_mala, steps=1000), in_axes=(0, 0))
+
+        mala_samples = mala(key_mala, init_samples).reshape(-1, dim)
+        bkjx_mala_samples, *_ = bkjx_mala(split(key_bkjx, n_chains), init_samples)
+        rwm_samples = rwmh(key_rwm, init_samples).reshape(-1, dim)
+        target_samples = target.sample(key_target, (1000,))
+
+        plt.figure(figsize=(10, 5))
+        # plt.scatter(rwm_samples[:, 0], rwm_samples[:, 1], label='rwm samples')
+        plt.scatter(bkjx_mala_samples[:,0], bkjx_mala_samples[:, 1], label = 'bkjx samples')
+        plt.scatter(mala_samples[:, 0], mala_samples[:, 1], label='mala samples')
+        plt.scatter(target_samples[:, 0], target_samples[:, 1], label='target samples', alpha=.3)
+        plt.legend()
+        plt.show()
