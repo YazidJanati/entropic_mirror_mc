ff3e9bb64157d787a180547b204cd84a7052927c
yazidjanati <janati.yazid@gmail.com>
2023-08-28 18:28:43 +0200

    emc working

--- entropic_mirror_mc.py
+++ entropic_mirror_mc.py
@@ -1,6 +1,7 @@
 import jax.numpy as jnp
+from jax import jit
 from jax.lax import fori_loop
-from jax.random import split, categorical, uniform, normal
+from jax.random import split, categorical, uniform, normal, bernoulli
 from numpyro.distributions import MixtureGeneral, Categorical
 from gaussian_mixture import Gaussian_Mixture, params_to_gm
 from gaussian_mixture import train as train_gm
@@ -15,50 +16,48 @@ class MCMC_kernel(NamedTuple):
     n_chains: int
 
 
-def emc(key, logpdf, n_train, n_samples, model, global_kernel, local_kernel, n_chains, heavy_distr,
+def emc(key, logpdf, pow_eps, n_train, n_samples, model, global_kernel, local_kernel, n_chains, heavy_distr,
         mixed_proposal_weights=jnp.array([.8, .2])):
     key_train, key_init_proposal = split(key, 2)
     if isinstance(model, Gaussian_Mixture):
         key_means, key_covs, key_log_weights = split(key_init_proposal, 3)
         init_proposal = params_to_gm(means=normal(key_means, (model.n_components, model.dim)),
-                                     covs=normal(key_covs, (model.n_components, model.dim, model.dim)),
+                                     covs=jnp.eye(model.dim)[jnp.newaxis,:].repeat(model.n_components, 0),
                                      log_weights=jnp.log(jnp.array([1 / model.n_components] * model.n_components)))
 
-        def train_func(key, samples):
+        def train_func(key, proposal, samples):
             params = train_gm(samples=samples, init_params="kmeans", n_components=model.n_components, key=key)
             return params_to_gm(*params)
 
     keys = split(key_train, n_train)
-    partial_emc_step = partial(emc_step, key=keys, n_samples=n_samples, logpdf=logpdf, global_kernel=global_kernel,
+    partial_emc_step = partial(emc_step, key=keys, pow_eps=pow_eps, n_samples=n_samples, logpdf=logpdf, global_kernel=global_kernel,
                                local_kernel=local_kernel,
                                train_func=train_func, heavy_distr=heavy_distr,
                                mixed_proposal_weights=mixed_proposal_weights)
     return fori_loop(0, n_train, partial_emc_step, init_proposal)
 
 
-def emc_step(i, key, n_samples, logpdf, proposal, global_kernel, local_kernel, train_func, heavy_distr,
+def emc_step(i, proposal, pow_eps, key, n_samples, logpdf, global_kernel, local_kernel, train_func, heavy_distr,
              mixed_proposal_weights):
-    key_empirical, key_resample, key_lker = split(key[i], 3)
+    key_empirical, key_resample, key_lker, key_train = split(key[i], 4)
     if heavy_distr is None:
         mixed_proposal = proposal
     else:
         mixed_proposal = MixtureGeneral(Categorical(mixed_proposal_weights), [proposal, heavy_distr])
 
-    empirical_iterate = empirical_update(key_empirical, logpdf, pow, n_samples, global_kernel, mixed_proposal)
+    empirical_iterate = empirical_update(key_empirical, logpdf, pow_eps, n_samples, global_kernel, mixed_proposal)
     samples = resample_empirical_update(key_resample, empirical_iterate, n_samples)
-    samples = local_kernel.sampler(keys=split(key_lker, local_kernel.n_chains),
-                                   state=samples,
-                                   steps=n_samples // global_kernel.n_chains)
-    return train_func(proposal, samples)
+    samples = local_kernel(keys=split(key_lker, n_samples), state=samples)
+    return train_func(key=key_train, proposal=proposal, samples=samples)
 
 
-def empirical_update(key, logpdf, pow, n_samples, global_kernel, prev_proposal):
+def empirical_update(key, logpdf, pow_eps, n_samples, global_kernel, prev_proposal):
     proposal_samples = prev_proposal.sample(key, (n_samples,))
-    proposal_log_weights = pow * (logpdf(proposal_samples) - prev_proposal.log_prob(proposal_samples))
-    global_kernel_samples = global_kernel.sampler(keys=split(key, global_kernel.n_chains),
-                                                  state=proposal_samples,
-                                                  steps=n_samples // global_kernel.n_chains)
-    global_kernel_log_weights = pow * (logpdf(global_kernel_samples) - prev_proposal.log_prob(global_kernel_samples))
+    proposal_log_weights = pow_eps * (logpdf(proposal_samples) - prev_proposal.log_prob(proposal_samples))
+    global_kernel_samples = global_kernel(keys=split(key, n_samples),
+                                          state=proposal_samples)
+    global_kernel_log_weights = pow_eps * (
+                logpdf(global_kernel_samples) - prev_proposal.log_prob(global_kernel_samples))
     global_kernel_lse, proposal_lse = logsumexp(global_kernel_log_weights), logsumexp(proposal_log_weights)
     alpha = cond((global_kernel_lse - jnp.log(n_samples) > 0),
                  lambda _: (global_kernel_lse - jnp.log(n_samples)) / (global_kernel_lse - proposal_lse),
@@ -68,15 +67,19 @@ def empirical_update(key, logpdf, pow, n_samples, global_kernel, prev_proposal):
 
 
 def resample_empirical_update(key, empirical_update, n_samples):
+    # TODO: make it more efficient by resampling then propagating
     key_resample_gker, key_resample_proposal, key_cat = split(key, 3)
     proposal_samples, proposal_log_weights, global_kernel_samples, global_kernel_log_weights, alpha = empirical_update
-    n_proposal_resamples = categorical(key_cat, alpha, shape=(n_samples,)).sum()
-    n_global_kernel_resamples = n_samples - n_proposal_resamples
-    global_kernel_resamples = resample(key_resample_gker, global_kernel_samples, global_kernel_log_weights,
-                                       n_global_kernel_resamples)
-    proposal_resamples = resample(key_resample_proposal, proposal_samples, proposal_log_weights, n_proposal_resamples)
-    return jnp.vstack([proposal_resamples, global_kernel_resamples])
-
+    samples = jnp.vstack([proposal_samples, global_kernel_samples])
+    log_weights = jnp.concatenate([alpha * proposal_log_weights, (1 - alpha) * global_kernel_log_weights])
+    resample_idxs = categorical(key_cat, log_weights, shape=(n_samples,))
+    return samples[resample_idxs, :]
+    # n_proposal_resamples = bernoulli(key_cat, p=alpha, shape=(n_samples,)).sum()
+    # n_global_kernel_resamples = n_samples - n_proposal_resamples
+    # global_kernel_resamples = resample(key_resample_gker, global_kernel_samples, global_kernel_log_weights,
+    #                                    n_global_kernel_resamples)
+    # proposal_resamples = resample(key_resample_proposal, proposal_samples, proposal_log_weights, n_proposal_resamples)
+    # return jnp.vstack([proposal_resamples, global_kernel_resamples])
 
 def resample(key, samples, log_weights, n_samples):
     idxs = categorical(key, log_weights, shape=(n_samples,))
--- tests/emc_test.py
+++ tests/emc_test.py
@@ -6,40 +6,46 @@ from jax.tree_util import Partial as partial
 from mcmc import random_walk_mh, unadjusted_langevin, RWM
 from targets import mog4_blockdiag_cov, mog25, mog2_blockdiag_cov
 from jax import grad, vmap
-from jax.random import split, PRNGKey, normal
+from jax.random import split, PRNGKey, normal, categorical
 from numpyro.distributions import MultivariateNormal, MixtureSameFamily, Categorical
 import matplotlib.pyplot as plt
 
 dim = 2
 # target = mog4_blockdiag_cov(dim=dim, mini_cov=jnp.eye(2))
-means = jnp.array([[0., 0.], [2., 2.]])
-covs = .2*jnp.eye(dim)[jnp.newaxis,:].repeat(2, 0)
+means = jnp.array([[0., 0.], [5., 5.]])
+covs = .2 * jnp.eye(dim)[jnp.newaxis, :].repeat(2, 0)
 target = MixtureSameFamily(Categorical(jnp.array([.5, .5])), MultivariateNormal(means, covs))
 rwm_params = RWM(cov=jnp.eye(dim))
 
-partial_rwm = partial(random_walk_mh, logpdf=target.log_prob, params=rwm_params, burn_in_steps=30)
-partial_ula = partial(unadjusted_langevin, grad_logpdf=grad(lambda x: target.log_prob(x)), burn_in_steps=30,
+partial_rwm = partial(random_walk_mh, logpdf=target.log_prob, params=rwm_params, burn_in_steps=100, steps=1)
+partial_ula = partial(unadjusted_langevin, grad_logpdf=grad(lambda x: target.log_prob(x)), burn_in_steps=100, steps=1,
                       step_size=5e-2)
-ula = vmap(lambda steps, keys, state: partial_ula(steps=steps, key=keys, init_state=state), in_axes=(None, 0, 0))
-rwm = vmap(lambda steps, keys, state: partial_rwm(steps=steps, key=keys, init_state=state), in_axes=(None, 0, 0))
-
-local_kernel = MCMC_kernel(sampler=lambda steps, keys, state: ula(steps, keys, state), n_chains=20)
-global_kernel = MCMC_kernel(sampler=lambda steps, keys, state: rwm(steps, keys, state), n_chains=20)
+global_kernel = lambda keys, state: vmap(partial_ula, in_axes=(0, 0))(keys, state).reshape(-1, state.shape[-1])
+local_kernel = lambda keys, state: vmap(partial_rwm, in_axes=(0, 0))(keys, state).reshape(-1, state.shape[-1])
 
 key_emc = PRNGKey(0)
 model = Gaussian_Mixture(dim=dim, n_components=2)
-proposal = emc(key_emc, logpdf=target.log_prob, n_train=10, n_samples=1000, model=model, global_kernel=global_kernel,
-               local_kernel=local_kernel, n_chains=20, heavy_distr=None)
 
-n_chains = 20
-key_init = PRNGKey(0)
-keys_ula = split(key_init, n_chains)
-init_state = normal(key_init, (n_chains, dim))
-target.log_prob(init_state)
-rwm_samples = ula(100, keys_ula, init_state).reshape(-1,dim)
+with jax.disable_jit(False):
+    proposal = emc(key_emc, pow_eps=.5, logpdf=target.log_prob, n_train=10, n_samples=1000, model=model,
+                   global_kernel=global_kernel,
+                   local_kernel=local_kernel, n_chains=20, heavy_distr=None)
 
-plt.figure(figsize=(10,5))
-plt.scatter(rwm_samples[:,0], rwm_samples[:,1])
+key_proposal, key_target = split(key_emc, 2)
+proposal_samples = proposal.sample(key_proposal, (1000,))
+target_samples = target.sample(key_target, (1000,))
+
+plt.figure(figsize=(10, 5))
+plt.scatter(proposal_samples[:, 0], proposal_samples[:, 1])
+plt.scatter(target_samples[:, 0], target_samples[:, 1])
 plt.show()
-
-
+# n_chains = 20
+# key_init = PRNGKey(0)
+# keys_ula = split(key_init, n_chains)
+# init_state = normal(key_init, (n_chains, dim))
+# target.log_prob(init_state)
+# rwm_samples = ula(100, keys_ula, init_state).reshape(-1,dim)
+#
+# plt.figure(figsize=(10,5))
+# plt.scatter(rwm_samples[:,0], rwm_samples[:,1])
+# plt.show()
